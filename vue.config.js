0f173d41a835179f3e6aae3d4447029eabeae1d2
thatjsprof <davidrums65@gmail.com>
2021-10-26 15:33:08 +0100

    setup for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/sorting_visualiser/" : "/",
+  transpileDependencies: ["vuetify"],
+};
